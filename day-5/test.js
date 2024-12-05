2d0ddea8e06c27e307b830255e65894f5f7ff3bc
posejdon256 <anna@gyfted.me>
2024-12-05 06:46:46 +0100

    day 5

--- day-5/test.js
+++ day-5/test.js
@@ -7,19 +7,72 @@ const getInputs = async (testString) => {
     crlfDelay: Infinity,
   });
 
-  const lines = [];
+  const orderMap = {};
+  const prints = [];
   for await (const line of rl) {
-    lines.push(line);
+    if (line.split(",").length !== 1) {
+      prints.push([...line.split(",").map((x) => parseInt(x))]);
+    } else {
+      const order = line.split("|").map((x) => parseInt(x));
+      if (!orderMap[order[0]]) {
+        orderMap[order[0]] = [];
+      }
+      orderMap[order[0]].push(order[1]);
+    }
   }
-  return { lines };
+  return { orderMap, prints };
+};
+
+const isOrderOk = (order = [1], orderMap = { 10: [1] }) => {
+  for (let i = 0; i < order.length; i++) {
+    const element = order[i];
+    const orderBeforeCrrent = order.slice(0, i);
+    if (orderBeforeCrrent.find((x) => orderMap[element]?.some((y) => x === y))) {
+      return false;
+    }
+  }
+  return true;
 };
 
 const part1 = async () => {
-  const { lines } = await getInputs("test.txt");
+  const { orderMap, prints } = await getInputs("test-2.txt");
+  let sum = 0;
+  prints.forEach((print) => {
+    if (isOrderOk(print, orderMap)) {
+      sum += print[Math.floor(print.length / 2)];
+    }
+  });
+  console.log(sum);
+};
+
+const fixOrder = (order = [1], orderMap = { 10: [1] }) => {
+  let i = 0;
+  const orderCopy = [...order];
+  while (i !== orderCopy.length) {
+    const element = orderCopy[i];
+    const orderBeforeCrrent = orderCopy.slice(0, i);
+    if (orderBeforeCrrent.find((x) => orderMap[element]?.some((y) => x === y))) {
+      orderCopy.splice(i, 1);
+      orderCopy.splice(0, 0, element);
+      i = 0;
+    } else {
+      i++;
+    }
+  }
+  return orderCopy;
 };
 
 const part2 = async () => {
-  const { lines } = await getInputs("test2.txt");
+  const { orderMap, prints } = await getInputs("test-2.txt");
+  let sum = 0;
+  prints.forEach((print) => {
+    let printCopy = [...print];
+    if (!isOrderOk(print, orderMap)) {
+      printCopy = fixOrder(printCopy, orderMap);
+      sum += printCopy[Math.floor(printCopy.length / 2)];
+    }
+  });
+  console.log(sum);
 };
 
-part1();
+part2();
