406415cba6444572f74659ee9df1b8a4b1b94d50
posejdon256 <anna@gyfted.me>
2024-12-05 16:19:02 +0100

    template folder

--- 2023-day-2/test.js
+++ 2023-day-2/test.js
@@ -13,15 +13,70 @@ const getInputs = async (testString) => {
   }
   return { lines };
 };
+const getColorNumber = (regex, part) => {
+  const color = parseInt(part.match(regex)?.toString().split(" ")[0]);
+  if (isNaN(color)) {
+    return -1;
+  }
+  return color;
+};
+const doesItMatch = (regex, part, max) => {
+  const color = getColorNumber(regex, part);
+  if (color <= max) {
+    return true;
+  }
+  return false;
+};
 
 const part1 = async () => {
-  const { lines } = await getInputs("test.txt");
-  return 0;
+  const { lines } = await getInputs("test2.txt");
+  const max = { red: 12, green: 13, blue: 14 };
+  let sum = 0;
+  lines.forEach((line) => {
+    const gameNumberReg = /[0-9]*\:/g;
+    const gameNumber = parseInt(line.match(gameNumberReg).toString().split(":")[0]);
+
+    const parts = line.split(";");
+    let bad = false;
+    parts.forEach((part) => {
+      if (!doesItMatch(/[0-9]* red/g, part, max.red)) {
+        bad = true;
+      }
+      if (!doesItMatch(/[0-9]* green/g, part, max.green)) {
+        bad = true;
+      }
+      if (!doesItMatch(/[0-9]* blue/g, part, max.blue)) {
+        bad = true;
+      }
+    });
+    if (!bad) {
+      sum += gameNumber;
+    }
+    console.log(parts);
+  });
+  return sum;
 };
 
 const part2 = async () => {
   const { lines } = await getInputs("test2.txt");
-  return 0;
+  let sum = 0;
+  lines.forEach((line) => {
+    const parts = line.split(";");
+    let gamePower = 0;
+    let maxInGame = { red: -1, green: -1, blue: -1 };
+    parts.forEach((part) => {
+      const red = getColorNumber(/[0-9]* red/g, part);
+      const green = getColorNumber(/[0-9]* green/g, part);
+      const blue = getColorNumber(/[0-9]* blue/g, part);
+      maxInGame.red = Math.max(maxInGame.red, red);
+      maxInGame.blue = Math.max(maxInGame.blue, blue);
+      maxInGame.green = Math.max(maxInGame.green, green);
+    });
+    gamePower = maxInGame.red * maxInGame.green * maxInGame.blue;
+    sum += gamePower;
+    console.log(gamePower);
+  });
+  return sum;
 };
 
 const main = async () => {
