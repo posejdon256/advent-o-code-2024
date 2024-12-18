4623f0c339513ee7594761ffe5e5ef969d1e2807
Anna Winiarczuk <aniutka.pop@gmail.com>
2024-12-18 20:35:23 +0100

    update template

---
+++ helpers/print.js
@@ -0,0 +1,15 @@
+const printSolution = async (part1, part2) => {
+  console.time("Time 1");
+  const part1Val = await part1();
+  console.timeEnd("Time 1");
+  console.log("Part 1", part1Val);
+
+  console.time("Time 2");
+  const part2Val = await part2();
+  console.timeEnd("Time 2");
+  console.log("Part 2", part2Val);
+};
+
+module.exports = {
+  printSolution,
+};
