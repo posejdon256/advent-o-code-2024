7f0a1b669d0717bae6a26437c5eeb37df2d5e094
Anna Winiarczuk <aniutka.pop@gmail.com>
2024-12-19 20:05:54 +0100

    refactor

--- day-19/helpers.js
+++ day-19/helpers.js
@@ -10,21 +10,17 @@ const buildPatternsMap = (patterns) => {
 
 const comparePatternAndWord = (pattern = "sss", l = 0, r = 100, word) => {
   i = 0;
-  // console.log(word, pattern, word[l], pattern[i], l, r);
   while (l < r) {
     if (word[l] !== pattern[i]) {
-      //  console.log("false");
       return false;
     }
     l++;
     i++;
   }
-  // console.log("true");
   return true;
 };
 
 const canCreateLayout = (word = "adasdasdas", ind = 0, patterns = ["sss"], checked = {}) => {
-  //console.log(word, ind);
   if (ind === word.length) {
     return true;
   }
@@ -44,7 +40,6 @@ const canCreateLayout = (word = "adasdasdas", ind = 0, patterns = ["sss"], check
 };
 
 const canCreateLayoutV2 = (word = "adasdasdas", ind = 0, patterns = ["sss"], checked = {}) => {
-  //console.log(word, ind);
   if (ind === word.length) {
     return 1;
   }
@@ -64,47 +59,8 @@ const canCreateLayoutV2 = (word = "adasdasdas", ind = 0, patterns = ["sss"], che
   return sum;
 };
 
-const findAllPlacesForPatterns = (patterns = ["asa"], word) => {
-  const places = {};
-  for (let i = 0; i < patterns.length; i++) {
-    let index = 0;
-    let startIndex = 0;
-    places[patterns[i]] = new Array(word.length).fill(0);
-    while ((index = word.indexOf(patterns[i], startIndex)) > -1) {
-      places[patterns[i]][index] = patterns[i].length;
-      startIndex = index + patterns[i].length;
-    }
-  }
-  // console.log(word, places);
-  return places;
-};
-
-const findAllPaths = (map = { rww: [0, 1] }, ind = 0, size, cache = {}) => {
-  if (ind >= size) {
-    return 1;
-  }
-  let sum = 0;
-  const rec = Object.entries(map)
-    .map(([key, value]) => {
-      return { key, value: value[ind] };
-    })
-    .filter((x) => x.value >= 1);
-  for (let i = 0; i < rec?.length; i++) {
-    const cacheIndex = `${rec[i].key}_${ind + rec[i].value}`;
-    if (cache[cacheIndex]) {
-      sum += cache[cacheIndex];
-    } else {
-      const value = findAllPaths(map, ind + rec[i].value, size, cache);
-      cache[cacheIndex] = value;
-      sum += value;
-    }
-  }
-  return sum;
-};
 module.exports = {
   buildPatternsMap,
   canCreateLayout,
   canCreateLayoutV2,
-  findAllPlacesForPatterns,
-  findAllPaths,
 };
--- day-19/task.js
+++ day-19/task.js
@@ -1,16 +1,14 @@
 const { checkUnits } = require("./unit-tests");
 const { printSolution } = require("../helpers/print");
 const { getInputs } = require("./input");
-const { findAllPlacesForPatterns, findAllPaths, findAllPathsV2, canCreateLayout, canCreateLayoutV2 } = require("./helpers");
+const { canCreateLayout, canCreateLayoutV2 } = require("./helpers");
 
 const part1 = async () => {
   const { words, patterns, maxPatternLength } = await getInputs("test2.txt");
   let sum = 0;
   for (let i = 0; i < words.length; i++) {
     const _patterns = patterns.filter((x) => words[i].includes(x));
-    console.log(patterns.length, _patterns.length);
     if (canCreateLayout(words[i], 0, patterns, {})) {
-      // console.log("I", i);
       sum++;
     }
   }
@@ -22,19 +20,14 @@ function onlyUnique(value, index, array) {
 }
 
 const part2 = async () => {
-  const { words, patterns: _patterns, maxPatternLength } = await getInputs("test2.txt");
+  const { words, patterns: _patterns } = await getInputs("test2.txt");
   let sum = 0;
   for (let i = 0; i < words.length; i++) {
     const patterns = _patterns.filter(onlyUnique);
-    const map = findAllPlacesForPatterns(patterns, words[i]);
     if (!canCreateLayout(words[i], 0, patterns, {})) {
-      console.log("Not", i);
       continue;
     }
-    // console.log(map);
-    const _mmm = {};
-    const res = canCreateLayoutV2(words[i], 0, patterns, _mmm);
-    console.log("I", res);
+    const res = canCreateLayoutV2(words[i], 0, patterns, {});
     sum += res;
   }
   return sum;
